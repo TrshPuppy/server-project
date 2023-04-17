e05d3e04e6f06f817abfbd47efd4ae3af94850d5
Rose Pineau <rosepineau@protonmail.com>
2023-04-17 19:18:30 -0400

    Init.

---
+++ server_thing.py
@@ -0,0 +1,8 @@
+from flask import Flask as trsh_puppy_is_learning_python # Flas -@Gaped
+from flask import make_response
+
+danger_fart = trsh_puppy_is_learning_python("flask_thing") # app - @Packer
+
+@danger_fart.route("/", methods=["GET"])
+def hello_world():
+    return "<p>Hello, World!</p>"

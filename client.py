71a578d23d2a25a45397256c799fb7ee4ed23ea2
trshpuppy <trshpuppy@proton.me>
2023-05-01 23:23:03 -0400

    client and server can handle headers.

--- client.py
+++ client.py
@@ -1,13 +1,89 @@
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 5000  # The port used by the server
+PORT = 5001  # The port used by the server
 
-message = "tiddies"
+
+class Bananananananananas:  # Message - @GapedBrain
+    def __init__(self, ID, mode, sock, host, port):
+        self.ID = ID
+        self.mode = mode
+        self.suck = sock  # self.sock - @rulerlefi
+        self.addr = [host, port]
+        self.header_one = None
+        self.header_two = None
+        self.message_bytes = b""
+        self.message_string = ""
+        self.message_complete = False
+
+    def _read(self, bytes_to_read):
+        try:
+            data_chunk = self.suck.recv(bytes_to_read)
+            print(f"try statement {data_chunk}")
+        except:
+            print("except in _read try block")
+            return f""
+        else:
+            if data_chunk:
+                self.message_bytes += data_chunk
+            return data_chunk
+
+    def read(self):
+        if self.mode == "w":
+            return
+
+        header_one = self._read(2)
+        if header_one:
+            self.process_header_one(header_one)
+
+        if self.header_one != None:
+            self._read()
+
+        if self.header_one != None:
+            next_chunk = self.suck.recv(2)
+            print(f"next chunk ({next_chunk}) and length {len(next_chunk)}")
+
+    def process_header_one(self, header):
+        # this header should tell us how long the next header is
+        # header_length = 2
+        self.header_one = header.decode("utf-8", "replace")
+        print(f"length of tiddies {len(self.header_one)}")
+
+        return
+
+
+# these headers need IDs eventually
+header_one_data = 4
+header_one = chr(header_one_data) + chr(0)
+
+header_two_data = 6
+
+illillllllillllllllll = chr(header_two_data)  # @zoogod5000 "header_two"
+for byte in range(header_one_data - 1):
+    illillllllillllllllll += chr(0)
+
+cheese_data = "tiddie"  # @mezmiro "message"
+cheese = cheese_data
+
+
+entire_message = header_one + illillllllillllllllll + cheese
+print(entire_message)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(message.encode("utf-8", "strict"))
+    s.sendall(entire_message.encode("utf-8", "strict"))
+    s.sendall(entire_message.encode("utf-8", "strict"))
     data = s.recv(1024)
 
 print(f"Received {data!r}")
+
+
+"""
+fake message:
+
+4 : 6 : tiddies
+
+"46tiddies"
+
+
+"""
--- server.py
+++ server.py
@@ -9,7 +9,7 @@
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 5000  # Port to listen on (non-privileged ports are > 1023)
+PORT = 5001  # Port to listen on (non-privileged ports are > 1023)
 
 
 class Bananananananananas:  # Message - @GapedBrain
@@ -24,38 +24,66 @@ class Bananananananananas:  # Message - @GapedBrain
         self.message_string = ""
         self.message_complete = False
 
-    def _read(self):
+    def _read(self, bytes_to_read):
         try:
-            data_chunk = self.suck.recv(4096)
+            data_chunk = self.suck.recv(bytes_to_read)
         except:
             print("except in _read try block")
-            pass
+            return
         else:
             if data_chunk:
-                self.message_bytes += data
+                self.message_bytes += data_chunk
+            return data_chunk
 
     def read(self):
         if self.mode == "w":
             return
 
-        self._read()
+        header_one = self._read(2)
+        if header_one:
+            self.process_header_one(header_one)
 
-        if self.header_one == None and self.message_bytes >= 2:
-            header = self.message_bytes
-            self.process_header_one(header)
+        header_two = None
+        if self.header_one != None:
+            header_2_length = self.header_one
+            header_two = self._read(header_2_length)
+
+        if header_two:
+            self.process_header_two(header_two)
+
+        data_received = None
+        if self.header_two != None:
+            data_length = self.header_two
+            data_received = self._read(data_length)
+
+        if data_received:
+            self.process_data_received(data_received)
 
     def process_header_one(self, header):
-        header_length = 2
+        # this header should tell us how long the next header is
+        self.header_one = header[0]
+        return
 
-        self.header_one = header.decode("utf-8", "replace")
-        print(self.header_one)
+    def process_header_two(self, header):
+        # this header should tell us how long the data is
+        self.header_two = header[0]
+        return
 
+    def process_data_received(self, data):
+        self.message_string = data.decode("utf-8")
+        if len(self.message_string) == self.header_two:
+            self.message_complete = True
+        else:
+            # do something here about checking for more data
+            print("self.message_string length != entire predicted length")
+            pass
         return
 
 
 def start_socket_listen():
     message_ID = 0
     Socktopus = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket
+    Socktopus.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     Socktopus.bind((HOST, PORT))
     Socktopus.listen()
 
@@ -63,19 +91,24 @@ def start_socket_listen():
     with connection:
         print(f"Connection to: {address}")
         while True:
-            if connection.recv(1024):
-                message = Bananananananananas(
-                    message_ID, "r", Socktopus, address[0], address[1]
-                )
-                if message.message_complete:
-                    print("message complete: " + message)
-                    connection.sendall(message.message_string.encode("utf-8", "strict"))
-                    Socktopus.close()
-                    message_ID += 1
-                    break
+            message = Bananananananananas(
+                message_ID, "r", connection, address[0], address[1]
+            )
+            message.read()
+            if message.message_complete:
+                print("Data received in full.")
+                connection.sendall(message.message_string.encode("utf-8", "strict"))
+                Socktopus.close()
+                message_ID += 1
+                break
+            if KeyboardInterrupt():
+                Socktopus.close()
+                break
 
 
 start_socket_listen()
+
+
 """
  message:
     properties:
